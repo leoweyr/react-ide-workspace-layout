a0c7f05a11672e55dea86a4e78d039050592e6dd
leoweyr <contact@leoweyr.com>
2026-02-24 10:10:11 +0800

    style: enforce comment standards in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 
-// https://vitejs.dev/config/
+// See https://vitejs.dev/config/.
 export default defineConfig({
     plugins: [
         react(),
