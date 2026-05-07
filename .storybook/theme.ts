78c30c8c3371f4ccce2b2e83be8e5cd2b79cbcea
leoweyr <contact@leoweyr.com>
2026-05-07 21:46:15 +0800

    chore: customize Storybook branding

---
+++ .storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+
+export default create({
+    base: 'light',
+    brandTitle: 'React IDE Workspace Layout',
+    brandUrl: 'https://github.com/leoweyr/react-ide-workspace-layout',
+    brandImage: './icon.svg',
+    brandTarget: '_self',
+});
